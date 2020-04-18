70a6efa1733f8f59f5074c39399b126255c7956c
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-18 14:16:03 +0100

    Fix run script

--- run.sh
+++ run.sh
@@ -7,12 +7,13 @@ DATE=$(date "+%Y-%m-%d")
 DATE="2020-04-16"
 
 echo "*** ECDC data"
-# ./orderly run ecdc date=$DATE
+./orderly run ecdc date=$DATE
 
 COUNTRIES=$(grep -E '^[A-Z]{3}\s*' countries)
 
 # Parallel
-echo $COUNTRIES | parallel -j 4 ./orderly run lmic_reports iso3c={} date=$DATE
+echo $COUNTRIES |
+    parallel --citation -j 4 ./orderly run lmic_reports iso3c={} date=$DATE
 
 # Serial
 # for ISO in $COUNTRIES; do
