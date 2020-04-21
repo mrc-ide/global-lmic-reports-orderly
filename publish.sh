ce294ae1b6cefa87b06e90fbff41335ab9be3011
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-21 12:58:20 +0100

    Support for staging server and deploy keys

--- publish.sh
+++ publish.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 DOCS_DIR=gh-pages
 VERSION=$(git rev-parse --short HEAD)
-REMOTE_URL=git@github.com:mrc-ide/global-lmic-reports.git
 
-if [ -d $DOCS_DIR ]; then
-    git -C $DOCS_DIR pull
+WHERE=$1
+
+if [[ $WHERE == "staging" ]]; then
+    REMOTE_URL=git@github.com:mrc-ide/global-lmic-reports-staging.git
+elif [[ $WHERE == "production" ]]; then
+    REMOTE_URL=git@github.com:mrc-ide/global-lmic-reports.git
 else
-    git clone $REMOTE_URL $DOCS_DIR
+    echo "Usage: ./publish.sh staging|production"
+    exit 1
+fi
+
+if [ -f .ssh/id_rsa ]; then
+    echo "Using deploy key"
+    ## NOTE, uses one directory above the root
+    export GIT_SSH_COMMAND="ssh -i ../.ssh/id_rsa"
 fi
 
 rm -rf ${DOCS_DIR}/.git
