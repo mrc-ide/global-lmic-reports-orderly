7e74d82a0f530dcd37958a8594ee97e210481620
OJWatson <oj.watson@hotmail.co.uk>
2020-05-07 00:42:48 +0100

    Add in google mobility data and format

--- src/brt_google_mobility/script.R
+++ src/brt_google_mobility/script.R
@@ -1,70 +1,122 @@
+## -----------------------------------------------------------------------------
+## Step 0: Function Prep
+## -----------------------------------------------------------------------------
+
 # Loading Required Libraries
 library(tidyverse); library(gbm); library(dismo); library(conflicted); library(gtools); library(lubridate)
 conflict_prefer("select", "dplyr")
 conflict_prefer("filter", "dplyr")
 conflict_prefer("area", "patchwork")
 
+download_url <- function(url) {
+  tryCatch({
+    tf <- tempfile()
+    code <- download.file(url, tf, mode = "wb")
+    if (code != 0) {
+      stop("Error downloading google csv")
+    }
+  },
+  error = function(e) {
+    stop(sprintf("Error downloading file '%s': %s, please check %s",
+                 url, e$message))
+  })
+  return(tf)
+}
+
+match_clean <- function(a,b, quiet=TRUE){
+  a <- gsub("[[:punct:][:space:]]","",tolower(stringi::stri_trans_general(a, "latin-ascii")))
+  b <- gsub("[[:punct:][:space:]]","",tolower(stringi::stri_trans_general(b, "latin-ascii")))
+  ret <- match(a,b)
+  if(sum(is.na(ret)>0)){
+    dists <- stringdist::seq_distmatrix(lapply(a,utf8ToInt),lapply(b,utf8ToInt))
+    ret[is.na(ret)] <- apply(dists[which(is.na(ret)),,drop=FALSE],1,which.min)
+    if(!quiet){
+      return(unique(cbind(a,b[ret])))
+    }
+  }
+  return(ret)
+}
+
+## -----------------------------------------------------------------------------
+## Step 1: Data Loading
+## -----------------------------------------------------------------------------
+
+date <- as.Date(date)
+
 # Loading Google Mobility Data
-mob <- read.csv("data/Google_Mobility_Data.csv", stringsAsFactors = FALSE) %>%
+goog_tf <- download_url("https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv")
+goog <- read.csv(goog_tf, stringsAsFactors = FALSE)
+goog$iso3c <- countrycode::countrycode(goog$country_region, "country.name", "iso3c")
+
+
+# N.B. The date format changed recently in Google and may change again. Look out for errors related to this
+mob <- goog %>%
   filter(sub_region_1 == "") %>%
   mutate(overall = 1/4 * retail_and_recreation_percent_change_from_baseline +
            1/4 * grocery_and_pharmacy_percent_change_from_baseline + 
            1/4 * transit_stations_percent_change_from_baseline + 
            1/4 * workplaces_percent_change_from_baseline) %>%
-  mutate(date = as.Date(date, format="%d/%m/%Y")) %>%
-  select(country_region, date, overall)
+  mutate(date = as.Date(date, format="%Y-%m-%d")) %>%
+  select(country_region, iso3c, date, overall)
 
-# Loading World Bank Metadata
-wb_metadata <- read.csv("data/World_Bank_Country_Metadata.csv", fileEncoding="UTF-8-BOM") %>%
+# Loading World Bank Metadata (Charlie can update to url as needed)
+wb_metadata <- read.csv("World_Bank_Country_Metadata.csv", fileEncoding="UTF-8-BOM") %>%
   rename(ISO = country_code) %>%
   select(ISO, income_group, region) %>%
   filter(region != "")
 
 # Loading in ACAPs Data
-ACAPs_measure <- read.csv("data/ACAPS_Intervention_Data.csv") %>%
+acap_tf <- download_url("https://www.acaps.org/sites/acaps/files/resources/files/acaps_covid19_goverment_measures_dataset.xlsx")
+acap <- readxl::read_excel(acap_tf, progress = FALSE, sheet = "Database",)
+ACAPs_measure <- acap %>%
   rename(country = COUNTRY, measure = MEASURE, type = LOG_TYPE, date = DATE_IMPLEMENTED) %>%
-  select(country, ISO, measure, type, date) %>%
-  filter(country != "", measure != "", type != "") %>%
+  select(ISO, measure, type, date) %>%
+  filter(measure != "", type != "") %>%
   mutate(measure = as.numeric(factor(measure)), type = as.numeric(factor(type))) %>%
-  mutate(combined = paste0(type, "_", measure)) %>%
-  mutate(date = as.Date(date, format="%d/%m/%Y")) %>%
-  select(country, ISO, date, combined) %>%
+  mutate(combined = as.factor(paste0("m_",type, "_", measure))) %>%
+  mutate(date = as.Date(date)) %>%
+  select(ISO, date, combined) %>%
   pivot_wider(names_from = combined, values_from = combined) %>%
   filter(!is.na(date))
 
+## -----------------------------------------------------------------------------
+## Step 2: Data Cleaning
+## -----------------------------------------------------------------------------
+
 # Renaming Columns of ACAPs Data and Inputting In Number of Each Measure Put In On Each Date
-measures <- colnames(ACAPs_measure)[-(1:3)]
+measures <- colnames(ACAPs_measure)[-(1:2)]
 for (i in 1:length(measures)) {
   index <- measures[i]
-  ACAPs_measure[, index] <- unlist(lapply(seq_along(ACAPs_measure$country), function(x) {
-    length <- length(ACAPs_measure[, index][[1]][[x]])
-  }))
-  # print(i)
+  ACAPs_measure[, index] <- unlist(lapply(ACAPs_measure[[index]], length))
 }
 
 # Tracking Cumulative Number of Each Type of Measure Implemented and Creating New Rows for Dates 
 # Not Present in Each Country
 new_ACAPs_measure <- ACAPs_measure %>%
-  group_by(country) %>%
-  arrange(country, date) %>%
-  mutate_at(vars(-country, -ISO, -date), funs(cumsum(.))) %>%    
+  group_by(ISO) %>%
+  arrange(date) %>%
+  mutate_at(vars(-ISO, -date), funs(cumsum(.))) %>%    
   complete(date = seq.Date(min(ACAPs_measure$date), max(ACAPs_measure$date), by = "days")) %>%
-  mutate_at(vars(-country, -ISO, -date), funs(replace(., row_number() == 1, 0))) 
+  mutate_at(vars( -ISO, -date), funs(replace(., row_number() == 1, 0))) 
 
 # Filling Those Newly Created Rows With the Value In the Row Above (As We're Tracking Cumulative)
-column_names <- colnames(new_ACAPs_measure)[-c(1:3)]
+column_names <- colnames(new_ACAPs_measure)[-c(1:2)]
 new_ACAPs_cat <- fill(new_ACAPs_measure, column_names, .direction = c("down"))
-new_ACAPs_cat <- fill(new_ACAPs_cat, "ISO", .direction = "updown")
 
 # Joining This Data to the World Bank and Google Mobility Data
 overall <- new_ACAPs_cat %>%
-  left_join(mob, by = c("country" = "country_region", "date" = "date")) %>% 
+  left_join(mob, by = c("ISO" = "iso3c", "date" = "date")) %>% 
   filter(!is.na(overall)) %>%
-  left_join(wb_metadata, by = "ISO")
+  left_join(wb_metadata, by = "ISO") %>% 
+  select(-country_region)
 
 overall_test <- overall %>%
-  ungroup(country) %>%
-  select(overall, everything(), -country, -ISO, -date)
+  ungroup(ISO) %>%
+  select(overall, everything(), -ISO, -date)
+
+## -----------------------------------------------------------------------------
+## Step 3: BRT
+## -----------------------------------------------------------------------------
 
 # Running the BRT 
 tree_complexity <- 8
@@ -73,7 +125,7 @@ max_trees <- 3000
 learning_rate <- 0.05
 x <- as.data.frame(overall_test)
 brt <- gbm.step(data = x, 
-                gbm.x = 2:66,
+                gbm.x = 2:ncol(x),
                 gbm.y = 1,
                 family = "gaussian", 
                 tree.complexity = tree_complexity, 
@@ -81,16 +133,88 @@ brt <- gbm.step(data = x,
                 bag.fraction = bag_fraction, 
                 max.trees = max_trees, n.folds = 5)
 
-# predicted <- predict.gbm(brt, x[, c(2:66)], n.trees = brt$gbm.call$best.trees, type = "response")
+# predicted <- predict.gbm(brt, x[, c(2:ncol(x))], n.trees = brt$gbm.call$best.trees, type = "response")
 # plot(overall$overall, predicted, ylim = c(-100, 20), xlim = c(-100, 20), pch = 20, cex = 2, ylab = "")
 
-# Get Countries Present in ACAPs But Missing From Google, Infer Google Mobility from ACAPs
-google_country_codes <- unique(overall$ISO)
-ACAPs_country_codes <- as.character(unique(ACAPs_measure$ISO))
-non_google_country_codes <- ACAPs_country_codes[!(ACAPs_country_codes %in% google_country_codes)]
-ACAPs_subset <- new_ACAPs_cat[new_ACAPs_cat$ISO %in% non_google_country_codes, ] %>%
-  left_join(wb_metadata, by = "ISO")
-predicted <- predict.gbm(brt, ACAPs_subset[, c(4:68)], n.trees = brt$gbm.call$best.trees, type = "response")
+# let's create the complete data set and predict where data is missing
+output_data <- new_ACAPs_cat %>%
+  left_join(mob, by = c("ISO" = "iso3c", "date" = "date")) %>% 
+  left_join(wb_metadata, by = "ISO") %>%
+  ungroup(ISO) %>%
+  select(overall, everything(), -country_region)
+predicted <- predict.gbm(brt, output_data[which(is.na(output_data$overall)), c(4:68)], n.trees = brt$gbm.call$best.trees, type = "response")
+output_data$observed <- !is.na(output_data$overall)
+output_data$overall[which(is.na(output_data$overall))] <- predicted
+output_data$all_overall <- predict.gbm(brt, output_data[, c(4:68)], n.trees = brt$gbm.call$best.trees, type = "response")
+
+## -----------------------------------------------------------------------------
+## Step 4: Data Formatting
+## -----------------------------------------------------------------------------
+
+## Format the data into the correct form the orderly task
+res <- select(output_data, ISO, date, overall, all_overall, observed, income_group) %>% 
+  mutate(overall = (overall+100)/100,
+         all_overall = (all_overall+100)/100) %>% 
+  rename(C = overall,
+         C_predict = all_overall,
+         iso3c = ISO) %>% 
+  group_by(iso3c) %>% 
+  mutate(C = if(sum(observed)>0) {
+    C/mean(C[which(observed)[1:5]])
+  } else {
+    C/mean(C[date < as.Date("2020-03-01")])  
+  } ,
+  continent = countrycode::countrycode(iso3c, "iso3c","continent"))
+
+res <- split.data.frame(res, res$iso3c)
+
+# Add in missing countries
+nms <- unique(squire::population$iso3c)
+res_no <- lapply(nms[!nms %in% names(res)], function(x) {
+  return(data.frame())  
+})
+names(res_no) <- nms[!nms %in% names(res)]
+
+# group them and apply the change calculations again after the data cleaning
+res <- append(res, res_no)
+res <- lapply(res, function(x) {
+  
+  if(nrow(x) > 0) {
+    x$R0 <- 3*x$C
+    return(as.data.frame(x))
+  } else {
+    return(x)
+  }
+})
+
+
+res <- lapply(res,function(x){
+  if(nrow(x) > 0) {
+  x <- x[as.Date(x$date) <= date,]
+  }
+  return(x)
+  
+})
+
+# make sure they all have a pre first switch date
+res <- lapply(res,function(x){
+  
+  if(length(unique(x$C)) == 1) {
+    x <- rbind(x[1,],x)
+    x$date[1] <- x$date[2]-1
+    x[1,"C"] <- 1
+    x$R0[1] <- 3
+  }
+  return(x)
+  
+})
+
+saveRDS(res, "google_brt.rds")
+
+## -----------------------------------------------------------------------------
+## Step 5: Data Validation / Plotting
+## -----------------------------------------------------------------------------
+
 
 # Plot Predicted Countries' Google Mobility
 # par(mfrow = c(5, 5), mar = c(5, 3, 1, 1))
