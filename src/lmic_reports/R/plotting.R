56506c712c88c8a0c4f0f7e02a8cdfea1aed3523
OJWatson <oj.watson@hotmail.co.uk>
2020-05-17 14:34:50 +0100

    and y axis + 1 for dots

--- src/lmic_reports/R/plotting.R
+++ src/lmic_reports/R/plotting.R
@@ -512,7 +512,7 @@ deaths_plot_single <- function(out, data, date_0, date = Sys.Date(),
     geom_point(data = out$scan_results$inputs$data, mapping = aes(x=date, y=deaths,shape="Reported")) +
     ggplot2::geom_vline(xintercept = date, linetype = "dashed") +
     ggplot2::theme_bw()  +
-    ggplot2::scale_y_continuous(expand = c(0,0), limits = c(0, ymax)) +
+    ggplot2::scale_y_continuous(expand = c(0,0), limits = c(0, ymax+1)) +
     ggplot2::scale_x_date(date_breaks = "1 week", date_labels = "%b %d",
                           limits = c(min(data$date[data$deaths>0]), date + forecast)) +
     ggplot2::scale_fill_manual(name = "", labels = rev(c("Estimated")),
