ba5ff689b8bc8f35149bf4e473dc995454e42118
OJWatson <oj.watson@hotmail.co.uk>
2020-06-12 12:42:02 +0100

    Add parallel as default for PMCMC runs

--- run_google_countries_pmcmc.sh
+++ run_google_countries_pmcmc.sh
@@ -12,8 +12,11 @@ SHORT_RUN=${2:-$DEFAULT_SHORT}
 DEFAULT_FULL_SCENARIOS="FALSE"
 FULL_SCENARIOS=${3:-$DEFAULT_FULL_SCENARIOS}
 
+DEFAULT_PARALLEL="TRUE"
+PARALLEL=${4:-$DEFAULT_PARALLEL}
+
 DEFAULT_HICs="FALSE"
-HICs=${4:-$DEFAULT_HICs}
+HICs=${5:-$DEFAULT_HICs}
 
 echo "*** Date: $DATE"
 
@@ -28,5 +31,5 @@ echo "*** Running country reports"
 
 # Parallel
 grep -E '^[A-Z]{3}\s*' countries | \
-parallel --progress -j 64 ./orderly run lmic_reports_google_pmcmc iso3c={} date=$DATE short_run=$SHORT_RUN full_scenarios=$FULL_SCENARIOS
+parallel --progress -j 64 ./orderly run lmic_reports_google_pmcmc iso3c={} date=$DATE short_run=$SHORT_RUN parallel=$PARALLEL full_scenarios=$FULL_SCENARIOS
 
