d2a74a24469dab6bcdb7babed0dda57ad11c3a0f
OJWatson <oj.watson@hotmail.co.uk>
2020-08-20 18:19:16 +0100

    and update the run scripts

--- run/run_extra/run_syria_under_reporting.sh
+++ run/run_extra/run_syria_under_reporting.sh
@@ -6,8 +6,8 @@ set -e
 TODAY=$(date "+%Y-%m-%d")
 DATE=${1:-$TODAY}
 
-DEFAULT_URBAN="FALSE"
-URBAN=${2:-$DEFAULT_URBAN}
+DEFAULT_POP="2079000"
+DAM_POP=${2:-$DEFAULT_POP}
 
 DEFAULT_PHO="TRUE"
 PHO=${3:-$DEFAULT_PHO}
@@ -30,4 +30,4 @@ echo "*** Hospital Normal Use: $HNU"
 
 # Parallel
 grep -E "*." rf.txt | \
-parallel --progress -j 11 ./orderly run syria_under_reporting reporting_fraction={} date=$DATE urban=$URBAN poorer_health_outcomes=$PHO city_age=$CA hosp_beds=$HOSP hospital_normal_use=$HNU
+parallel --progress -j 11 ./orderly run syria_under_reporting reporting_fraction={} date=$DATE dam_pop=$DAM_POP poorer_health_outcomes=$PHO city_age=$CA hosp_beds=$HOSP hospital_normal_use=$HNU
