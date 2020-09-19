2ef7a586e0b6ad955e0879df26970adaf3d7b887
OJWatson <oj.watson@hotmail.co.uk>
2020-09-19 19:37:11 +0100

    worldometers switch in

--- src/ecdc/script.R
+++ src/ecdc/script.R
@@ -128,3 +128,89 @@ jhu_data$dateRep <- jhu_data$date
 
 # save 
 saveRDS(jhu_data, "jhu_all.rds")
+
+
+# AND Worldometers
+
+
+# function to get data from worldometers
+get_country_data <- function(link, iso3c, name) {
+  
+  url <- paste0("https://www.worldometers.info/coronavirus/country/", link)
+  html <- xml2::read_html(url)
+  scrs <- rvest::html_nodes(html, "script")
+  hcs <- grep("Highcharts.chart", unlist(lapply(scrs, as.character)))
+  
+  text <- scrs[hcs]
+  
+  death_dat <- text[grep("coronavirus-deaths-linear", text)]
+  if(length(death_dat) == 0) {
+    
+    dates_d <- NA
+    deaths <- NA
+    
+  } else {
+    
+    txt <- xml2::xml_text(death_dat)
+    spl <- strsplit(txt, "\n")[[1]]
+    
+    dates_d <- spl[grep("categories", spl)][1]
+    dates_d <- strsplit(dates_d, "\"|,")[[1]][which(nchar(strsplit(dates_d, "\"|,")[[1]])==6)]
+    dates_d <- as.Date(dates_d, "%b %d")
+    deaths <- spl[grep("data", spl)[1]]
+    deaths <- tail(head(strsplit(deaths, ",|\\[|\\]")[[1]],-1),-1)
+    deaths <- suppressWarnings(as.numeric(deaths))
+    deaths <- c(0,diff(deaths))
+    
+  }
+  cases_dat <- text[grep("graph-cases-daily", text)]
+  txt <- xml2::xml_text(cases_dat)
+  spl <- strsplit(txt, "\n")[[1]]
+  
+  dates_c <- spl[grep("categories", spl)]
+  dates_c <- strsplit(dates_c, "\"|,")[[1]][which(nchar(strsplit(dates_c, "\"|,")[[1]])==6)]
+  dates_c <- as.Date(dates_c, "%b %d")
+  cases <- spl[grep("data", spl)[1]]
+  cases <- tail(head(strsplit(cases, ",|\\[|\\]")[[1]],-1),-1)
+  cases <- suppressWarnings(as.numeric(cases))
+  
+  df <- data.frame("dateRep" = dates_c, "cases" = cases)
+  df$deaths <- deaths[match(df$dateRep, dates_d)]
+  df$deaths[is.na(df$deaths)] <- 0
+  
+  df$countryterritoryCode <- iso3c
+  df$Region <- name
+  
+  return(df)
+  
+}
+
+# country names from worldometers
+wo <- "https://www.worldometers.info/coronavirus/#countries"
+wo <- xml2::read_html(wo) %>% rvest::html_nodes(".mt_a")
+
+# create country names and links
+countries <- xml2::xml_text(wo) 
+links <- gsub("country/|/","",rvest::html_attr(wo, "href"))
+iso3cs <- countrycode::countrycode(countries, "country.name.en", "iso3c", 
+                                   custom_match = c("CAR" = "CAR"))
+
+# df of args to run
+df_args <- data.frame(countries = countries, links = links, iso3cs = iso3cs)
+df_args <- unique(df_args)
+df_args <- na.omit(df_args)
+
+# loop over data needs
+dats <- lapply(seq_along(df_args$countries), function(i) {
+  get_country_data(df_args$links[i], df_args$iso3cs[i], df_args$countries[i])
+})
+df <- do.call(rbind, dats)
+
+# and fill in leading NAs
+df$cases[is.na(df$cases)] <- 0
+df$deaths[is.na(df$deaths)] <- 0
+
+# worldometers is a day ahead of ECDC - so to keep it all aligned
+df$dateRep <- df$dateRep + 1
+
+saveRDS(df, "worldometers_all.rds")
