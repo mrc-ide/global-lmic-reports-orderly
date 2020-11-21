9b12968d81877bd621be7e3224a08d01cadc62d3
OJWatson <oj.watson@hotmail.co.uk>
2020-11-21 12:29:45 +0000

    Wider random walks

--- src/lmic_reports_google_pmcmc_spline_np/R/report_grab.R
+++ src/lmic_reports_google_pmcmc_spline_np/R/report_grab.R
@@ -237,9 +237,9 @@ generate_draws_pmcmc_fitted <- function(out, pmcmc, burnin, n_chains, squire_mod
   
   # do we need to go up or down
   if(des_grad <= pred_grad) {
-    alters <- seq(0.025, 0.425, 0.025)
+    alters <- seq(0.025, 0.825, 0.025)
   } else {
-    alters <- seq(-0.025, -0.325, -0.025) # more conservative on increasing Rt
+    alters <- seq(-0.025, -0.625, -0.025) # more conservative on increasing Rt
   }
   
   # store our grads
