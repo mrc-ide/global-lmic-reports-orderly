938541b408ac062c58df102df675c710ed0676e3
OJWatson <oj.watson@hotmail.co.uk>
2020-11-21 18:17:20 +0000

    iceland updated

--- src/lmic_reports_google_pmcmc_spline_np/script.R
+++ src/lmic_reports_google_pmcmc_spline_np/script.R
@@ -55,7 +55,7 @@ if(sum(ecdc_df$deaths) > 0) {
   data$date <- as.Date(data$date)
 
   # Handle for countries that have eliminated and had reintroduction events
-  if (iso3c %in% c("MMR", "BLZ", "TTO", "BHS", "HKG", "ABW", "GUM", "ISL", )) {
+  if (iso3c %in% c("MMR", "BLZ", "TTO", "BHS", "HKG", "ABW", "GUM", "ISL")) {
     deaths_removed <- deaths_removed + sum(data$deaths[data$date < as.Date("2020-06-01")])
     data$deaths[data$date < as.Date("2020-06-01")] <- 0
   }
