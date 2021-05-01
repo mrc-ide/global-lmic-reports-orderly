40ec6a6328799327ba67eea624d75f5532e62746
OJWatson <oj.watson@hotmail.co.uk>
2021-05-01 05:07:50 +0100

    run_collate_vaccine added

--- help/staging_navbar.R
+++ help/staging_navbar.R
@@ -1,15 +1,23 @@
-wd <- "/home/oj/net/lmic_new/datadrive/lmic/testing/"
-setwd(wd)
 
-country_root <- list.files("gh-pages", full.names = TRUE)
-sapply(grep("html", country_root, value = TRUE),
-       xfun::gsub_file,"global-lmic-reports","global-lmic-reports-staging")
-country_root <- country_root[dir.exists(country_root)]
-sapply(country_root, function(x){list.files(x)})
-country_root <- country_root[-grep("data", country_root)]
-done <- sapply(file.path(country_root, "index.html"), function(x) {
-  if(file.exists(x)) {
-    xfun::gsub_file(x,"global-lmic-reports","global-lmic-reports-staging")
-  }
-})
-               
+staging_navbar <- function() {
+  
+  country_root <- list.files("gh-pages", full.names = TRUE)
+  sapply(grep("html", country_root, value = TRUE),
+         xfun::gsub_file,"global-lmic-reports","global-lmic-reports-staging")
+  country_root <- country_root[dir.exists(country_root)]
+  sapply(country_root, function(x){list.files(x)})
+  country_root <- country_root[-grep("data", country_root)]
+  done <- sapply(file.path(country_root, "index.html"), function(x) {
+    if(file.exists(x)) {
+      xfun::gsub_file(x,"global-lmic-reports","global-lmic-reports-staging")
+    }
+  })
+  
+}
+
+
+if (!interactive()) {
+  usage <- "Usage:\n./staging_navbar.R"
+  args <- docopt::docopt(usage)
+  staging_navbar()
+}
