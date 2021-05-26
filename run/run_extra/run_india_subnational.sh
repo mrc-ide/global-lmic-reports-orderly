14b413ed96c0e3c1d39956c31e2df6152a9f3310
OJWatson <oj.watson@hotmail.co.uk>
2021-05-27 00:09:07 +0100

    subnat checks

--- run/run_extra/run_india_subnational.sh
+++ run/run_extra/run_india_subnational.sh
@@ -12,24 +12,16 @@ REPLICATES=${2:-$DEFAULT_REPLICATES}
 DEFAULT_N_MCMC="100000"
 N_MCMC=${3:-$DEFAULT_N_MCMC}
 
-DEFAULT_HOSP_BEDS="10000000000"
-HOSP_BEDS=${4:-$DEFAULT_HOSP_BEDS}
-
-DEFAULT_ICU_BEDS="10000000000"
-ICU_BEDS=${5:-$DEFAULT_ICU_BEDS}
-
 echo "*** RF: $RF"
 echo "*** Replicates: $REPLICATES"
 echo "*** MCMC Iterations: $N_MCMC"
-echo "*** Hospital Beds: $HOSP_BEDS"
-echo "*** ICU Beds: $ICU_BEDS"
 
 # First batch of 18
 parallel -k echo ::: "Andaman and Nicobar Islands" "Andhra Pradesh" "Arunachal Pradesh" "Assam" "Bihar" "Chandigarh" "Chhattisgarh" "Dadra and Nagar Haveli and Daman and Diu" "Delhi" "Goa" "Gujarat" "Haryana" "Himachal Pradesh" "Jammu and Kashmir" "Jharkhand" "Karnataka" "Kerala" "Ladakh"> prov-file
 
 # Parallel
 grep -E "*." prov-file | \
-parallel --progress -j 18 ./orderly run india_sub_national state={} rf=$RF replicates=$REPLICATES n_mcmc=$N_MCMC hosp_beds=$HOSP_BEDS icu_beds=$ICU_BEDS
+parallel --progress -j 18 ./orderly run india_sub_national state={} rf=$RF replicates=$REPLICATES n_mcmc=$N_MCMC
 
 rm prov-file
 
@@ -38,6 +30,6 @@ parallel -k echo ::: "Lakshadweep" "Madhya Pradesh" "Maharashtra" "Manipur" "Meg
 
 # Parallel
 grep -E "*." prov-file | \
-parallel --progress -j 18 ./orderly run india_sub_national state={} rf=$RF replicates=$REPLICATES n_mcmc=$N_MCMC hosp_beds=$HOSP_BEDS icu_beds=$ICU_BEDS
+parallel --progress -j 18 ./orderly run india_sub_national state={} rf=$RF replicates=$REPLICATES n_mcmc=$N_MCMC
 
 rm prov-file
