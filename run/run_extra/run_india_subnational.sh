57f65ada3f3b86e138d49513688553fe6725c41c
OJWatson <oj.watson@hotmail.co.uk>
2021-05-27 13:21:00 +0100

    today in run bash

--- run/run_extra/run_india_subnational.sh
+++ run/run_extra/run_india_subnational.sh
@@ -13,24 +13,15 @@ DEFAULT_N_MCMC="100000"
 N_MCMC=${3:-$DEFAULT_N_MCMC}
 
 TODAY=$(date "+%Y-%m-%d")
-DATE=${1:-$TODAY}
+DATE=${4:-$TODAY}
 
 
 echo "*** RF: $RF"
 echo "*** Replicates: $REPLICATES"
 echo "*** MCMC Iterations: $N_MCMC"
 
-# First batch of 18
-parallel -k echo ::: "Andaman and Nicobar Islands" "Andhra Pradesh" "Arunachal Pradesh" "Assam" "Bihar" "Chandigarh" "Chhattisgarh" "Dadra and Nagar Haveli and Daman and Diu" "Delhi" "Goa" "Gujarat" "Haryana" "Himachal Pradesh" "Jammu and Kashmir" "Jharkhand" "Karnataka" "Kerala" "Ladakh"> prov-file
-
-# Parallel
-grep -E "*." prov-file | \
-parallel --progress -j 18 ./orderly run india_sub_national state={} rf=$RF replicates=$REPLICATES n_mcmc=$N_MCMC date=$DATE
-
-rm prov-file
-
-# Second batch of 18
-parallel -k echo ::: "Lakshadweep" "Madhya Pradesh" "Maharashtra" "Manipur" "Meghalaya" "Mizoram" "Nagaland" "Odisha" "Puducherry" "Punjab" "Rajasthan" "Sikkim" "Tamil Nadu" "Telangana" "Tripura" "Uttar Pradesh" "Uttarakhand" "West Bengal" > prov-file
+# Batch in 18
+parallel -k echo ::: "Andaman and Nicobar Islands" "Andhra Pradesh" "Arunachal Pradesh" "Assam" "Bihar" "Chandigarh" "Chhattisgarh" "Dadra and Nagar Haveli and Daman and Diu" "Delhi" "Goa" "Gujarat" "Haryana" "Himachal Pradesh" "Jammu and Kashmir" "Jharkhand" "Karnataka" "Kerala" "Ladakh" "Lakshadweep" "Madhya Pradesh" "Maharashtra" "Manipur" "Meghalaya" "Mizoram" "Nagaland" "Odisha" "Puducherry" "Punjab" "Rajasthan" "Sikkim" "Tamil Nadu" "Telangana" "Tripura" "Uttar Pradesh" "Uttarakhand" "West Bengal"> prov-file
 
 # Parallel
 grep -E "*." prov-file | \
