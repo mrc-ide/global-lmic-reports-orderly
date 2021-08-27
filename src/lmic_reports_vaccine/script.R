dda969cad8ed806098749cf97e4c9bad9581d1fc
Oliver Watson <ow813@dide.local>
2021-08-27 09:24:56 +0100

    vacc_inputs name error

--- src/lmic_reports_vaccine/script.R
+++ src/lmic_reports_vaccine/script.R
@@ -853,7 +853,7 @@ if(sum(ecdc_df$deaths) > 0) {
   time_period <- 365
 
   ## We need to know work out vaccine doses and efficacy going forwards
-  model_user_args <- extend_vaccine_inputs(vaccine_inputs, time_period, out)
+  model_user_args <- extend_vaccine_inputs(vacc_inputs, time_period, out)
 
   # Maintaining the current set of measures for a further 3 months
   maintain_scenario <- squire::projections(out,
