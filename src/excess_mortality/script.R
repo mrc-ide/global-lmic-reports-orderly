42ad2b68342165060f42cfc7e8c0c2df13d813cd
GBarnsley <g.barnsley@imperial.ac.uk>
2021-09-21 13:29:30 +0100

    Add checks to make empty files if no data available

--- src/excess_mortality/script.R
+++ src/excess_mortality/script.R
@@ -38,110 +38,119 @@ data2 <- data[data$iso3c == iso3c, ]
 df <- data2
 df$deaths <- as.integer(df$deaths)
 
-## c. Any other parameters needed to be worked out
-## -----------------------------------------------------------------------------
+#if we have no deaths then we do not proceed
 
-# check that we have this iso3c in squire
-if(!(iso3c %in% squire::population$iso3c)) {
-  stop("iso3c not found in squire")
-}
-country <- squire::population$country[match(iso3c, squire::population$iso3c)]
-pop <- squire::get_population(country)$n
-
-if(adjust_delta){
-  #open data from covariants
-  delta_characteristics <- readRDS("delta_characteristics.Rds") %>% ungroup()
-
-  #get data or impute if not there
-  if (iso3c %in% delta_characteristics$iso3c) {
-    this_iso3c <- iso3c
-    delta_characteristics <- delta_characteristics %>%
-      filter(iso3c == this_iso3c) %>%
-      select(where(~is.numeric(.x) | is.Date(.x)))
-  } else if (
-    countrycode::countrycode(iso3c, origin = "iso3c",
-                             destination = "un.regionsub.name") %in%
-    delta_characteristics$sub_region
-  ) {
-    this_sub_region <- countrycode::countrycode(iso3c,
-                                                origin = "iso3c", destination = "un.regionsub.name")
-    #we then use the median values of all countries in that sub region
-    delta_characteristics <- delta_characteristics %>%
-      filter(
-        sub_region == this_sub_region
-      ) %>%
-      summarise(across(
-        where(~is.numeric(.x) | is.Date(.x)),
-        ~median(.x, na.rm = T)
-      ))
-  } else if (
-    countrycode::countrycode(iso3c, origin = "iso3c",
-                             destination = "continent") %in%
-    delta_characteristics$continent
-  ) {
-    this_continent <- countrycode::countrycode(iso3c, origin = "iso3c",
-                                               destination = "continent")
-    #we then use the median values of all countries in that contient
-    delta_characteristics <- delta_characteristics %>%
-      filter(
-        continent == this_continent
-      ) %>%
-      summarise(across(
-        where(~is.numeric(.x) | is.Date(.x)),
-        ~median(.x, na.rm = T)
-      ))
-  } else{
-    #else we use the median values for the world
-    delta_characteristics <- delta_characteristics %>%
-      summarise(across(
-        where(~is.numeric(.x) | is.Date(.x)),
-        ~median(.x, na.rm = T)
-      ))
-  }
+if(nrow(df) == 0 | sum(df$deaths) == 0){
+  saveRDS(NULL, "res.rds")
+  ggsave("fitting.pdf",width=12, height=12,
+         NULL)
 } else{
-  #these settings should lead to no adjustment
-  delta_characteristics <- data.frame(
+  ## c. Any other parameters needed to be worked out
+  ## -----------------------------------------------------------------------------
+
+  # check that we have this iso3c in squire
+  if(!(iso3c %in% squire::population$iso3c)) {
+    stop("iso3c not found in squire")
+  }
+  country <- squire::population$country[match(iso3c, squire::population$iso3c)]
+  pop <- squire::get_population(country)$n
+
+  if(adjust_delta){
+    #open data from covariants
+    delta_characteristics <- readRDS("delta_characteristics.Rds") %>% ungroup()
+
+    #get data or impute if not there
+    if (iso3c %in% delta_characteristics$iso3c) {
+      this_iso3c <- iso3c
+      delta_characteristics <- delta_characteristics %>%
+        filter(iso3c == this_iso3c) %>%
+        select(where(~is.numeric(.x) | is.Date(.x)))
+    } else if (
+      countrycode::countrycode(iso3c, origin = "iso3c",
+                               destination = "un.regionsub.name") %in%
+      delta_characteristics$sub_region
+    ) {
+      this_sub_region <- countrycode::countrycode(iso3c,
+                                                  origin = "iso3c", destination = "un.regionsub.name")
+      #we then use the median values of all countries in that sub region
+      delta_characteristics <- delta_characteristics %>%
+        filter(
+          sub_region == this_sub_region
+        ) %>%
+        summarise(across(
+          where(~is.numeric(.x) | is.Date(.x)),
+          ~median(.x, na.rm = T)
+        ))
+    } else if (
+      countrycode::countrycode(iso3c, origin = "iso3c",
+                               destination = "continent") %in%
+      delta_characteristics$continent
+    ) {
+      this_continent <- countrycode::countrycode(iso3c, origin = "iso3c",
+                                                 destination = "continent")
+      #we then use the median values of all countries in that contient
+      delta_characteristics <- delta_characteristics %>%
+        filter(
+          continent == this_continent
+        ) %>%
+        summarise(across(
+          where(~is.numeric(.x) | is.Date(.x)),
+          ~median(.x, na.rm = T)
+        ))
+    } else{
+      #else we use the median values for the world
+      delta_characteristics <- delta_characteristics %>%
+        summarise(across(
+          where(~is.numeric(.x) | is.Date(.x)),
+          ~median(.x, na.rm = T)
+        ))
+    }
+  } else{
+    #these settings should lead to no adjustment
+    delta_characteristics <- data.frame(
+    )
+  }
+
+  ## -----------------------------------------------------------------------------
+  ## 2. Fit Model
+  ## -----------------------------------------------------------------------------
+
+  # fit model
+  res <- fit_spline_rt(
+    data = df,
+    country = country,
+    pop = pop,
+    n_mcmc = as.numeric(n_mcmc),
+    replicates = as.numeric(replicates),
+    model = model,
+    delta_characteristics = delta_characteristics
   )
+
+  ## -----------------------------------------------------------------------------
+  ## 3. Summarise model for ease of viewing outputs and goodness of fit
+  ## -----------------------------------------------------------------------------
+
+  # remove the output for memory and ease
+  output <- res$output
+  res$output <- NULL
+
+  # save output without output for memory
+  saveRDS(res, "res.rds")
+  res$output <- output
+
+  # make a series of quick plots so we can check fits easily afterwards
+  if (model == "SQUIRE") {
+    rtp <- rt_plot_immunity(res)
+  } else {
+    rtp <- rt_plot_immunity_vaccine(res)
+  }
+
+  dp <- dp_plot(res)
+  cdp <- cdp_plot(res)
+  ar <- ar_plot(res)
+
+  ggsave("fitting.pdf",width=12, height=12,
+         cowplot::plot_grid(rtp$plot + ggtitle(country),
+                            dp, cdp, ar, ncol = 1))
+
 }
-
-## -----------------------------------------------------------------------------
-## 2. Fit Model
-## -----------------------------------------------------------------------------
-
-# fit model
-res <- fit_spline_rt(
-  data = df,
-  country = country,
-  pop = pop,
-  n_mcmc = as.numeric(n_mcmc),
-  replicates = as.numeric(replicates),
-  model = model,
-  delta_characteristics = delta_characteristics
-)
-
-## -----------------------------------------------------------------------------
-## 3. Summarise model for ease of viewing outputs and goodness of fit
-## -----------------------------------------------------------------------------
-
-# remove the output for memory and ease
-output <- res$output
-res$output <- NULL
-
-# save output without output for memory
-saveRDS(res, "res.rds")
-res$output <- output
-
-# make a series of quick plots so we can check fits easily afterwards
-if (model == "SQUIRE") {
-  rtp <- rt_plot_immunity(res)
-} else {
-  rtp <- rt_plot_immunity_vaccine(res)
-}
-
-dp <- dp_plot(res)
-cdp <- cdp_plot(res)
-ar <- ar_plot(res)
-
-ggsave("fitting.pdf",width=12, height=12,
-       cowplot::plot_grid(rtp$plot + ggtitle(country),
-                          dp, cdp, ar, ncol = 1))
