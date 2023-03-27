a166e05d9105313b57b724f5d841615c0004ac10
GBarnsley <gregbarnsley@hotmail.co.uk>
2023-03-27 18:39:31 +0100

    remove pages + esft code

--- R/gh_fits.R
+++ R/gh_fits.R
@@ -1,33 +1,20 @@
-update_gh_fits <- function(ids){
+update_gh_fits <- function(ids, task) {
   repo <- here::here()
   destination_excess <- file.path(
     repo, "gh-fits",
     "excess_mortality"
   )
   excess_name <- "excess_out.Rds"
-  destination_reported <- file.path(
-    repo, "gh-fits",
-    "reported_deaths"
-  )
-  reported_name <- "excess_out.Rds"
   #upload to folder replacing existing files
   dir.create(destination_excess, recursive = TRUE, showWarnings = FALSE)
-  dir.create(destination_reported, recursive = TRUE, showWarnings = FALSE)
 
   #minimise RAM useage by loading and saving in turn
-  purrr::walk(ids, function(x){
-    silent <- tryCatch(readRDS(file.path(repo, "archive", task, x$id, excess_name)) %>%
+  purrr::walk(ids, function(x) {
+    tryCatch(readRDS(file.path(repo, "archive", task, x$id, excess_name)) %>%
     saveRDS(file.path(
       destination_excess,
               paste0(x$iso3c, ".Rds")
-    )), error = function(e){NULL}
-    )
-  tryCatch(
-    readRDS(file.path(repo, "archive", task, x$id, reported_name)) %>%
-      saveRDS(file.path(
-        destination_reported,
-        paste0(x$iso3c, ".Rds")
-      )), error = function(e){NULL}
+    )), error = function(e) NULL)
+    }
   )
-  })
 }
--- src/pages_404/script.R
+++
@@ -1,6 +0,0 @@
-orderly_id <- tryCatch(orderly::orderly_run_info()$id,
-                       error = function(e) "<id>") # bury this in the html, docx
-
-date <- as.Date(date)
-rmarkdown::render("404.Rmd", output_format = c("html_document"), 
-                  output_options = list(pandoc_args = c("--metadata=title:404")))
