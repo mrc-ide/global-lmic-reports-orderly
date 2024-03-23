03ac072f229fdac795eb6c45bb52c9dd57bf32d3
GBarnsley <gregbarnsley@hotmail.co.uk>
2024-03-23 14:43:05 +0000

    finalise updates to HPC code

--- hpc/submit_slurm.sh
+++ hpc/submit_slurm.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=COVID_Model_Fits_$[SLURM_ARRAY_TASK_ID]    # Job name
-#SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=gregory.barnsley@lshtm.ac.uk # Where to send mail
-#SBATCH --ntasks=10                    # Run on a single core
-#SBATCH --array=1-33%3
-#SBATCH --mem=80gb                     # Job memory request
-#SBATCH --time=24:00:00               # Time limit hrs:min:sec
-#SBATCH --output=log/COVID_Model_Fits_$[SLURM_ARRAY_TASK_ID].log   # Standard output and error log
-pwd; hostname; date
+# SBATCH --job-name=COVID_Model_Fits_%a    # Job name
+# SBATCH --mail-type=END,FAIL,ARRAY_TASKS          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+# SBATCH --mail-user=gregory.barnsley@lshtm.ac.uk # Where to send mail
+# SBATCH --ntasks=10                    # Run on a single core
+# SBATCH --array=1-7
+# SBATCH --mem=80gb                     # Job memory request
+# SBATCH --time=24:00:00               # Time limit hrs:min:sec
+# SBATCH --output=log/COVID_Model_Fits_%a.log   # Standard output and error log
+pwd; date
 
 source ~/.bashrc
 
 source activate R
+
+echo $SLURM_ARRAY_TASK_ID
 
 iso3c=$(awk -F',' -v i=1 -v j=$SLURM_ARRAY_TASK_ID 'NR==j {print $i}' "bundles.csv")
 id=$(awk -F',' -v i=2 -v j=$SLURM_ARRAY_TASK_ID 'NR==j {print $i}' "bundles.csv")
